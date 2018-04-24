cba563a34a6f302e62cb78f15976f68a6794a00c
unknown <dean4ta@gmail.com>
2018-04-24 11:46:18 -0400

    init postproc

--- code/main.py
+++ code/main.py
@@ -71,7 +71,7 @@ def get_fourier(data,HPF_size=60):
         HPF_size: High Pass Filter size of box to filter out
     '''
     r,c = int(data.shape[0]/2),int(data.shape[1]/2)
-    data = cv2.fastNlMeansDenoising(data,None,10,7,21)
+    #data = cv2.fastNlMeansDenoising(data,None,10,7,21)
     data = np.fft.fftshift(cv2.dft(np.float32(data),flags=cv2.DFT_COMPLEX_OUTPUT))
     data[r-HPF_size:r+HPF_size,c-HPF_size:c+HPF_size] = 0
     data = cv2.idft(np.fft.ifftshift(data))
@@ -87,16 +87,16 @@ def extract_features(data,win_y=15,win_x=15):
     features = np.zeros((N1,N2,(8+1)*D)).astype(np.uint8) # 8=n_bins +1 for center
     for i in range(N1-win_y):
         print('iter '+str(i))
-        windows = roll.window(data[i:win_y+i,:,0],(win_y,win_x)).astype(np.uint16)
-        windows = np.concatenate((windows,1*255+roll.window(data[i:win_y+i,:,1],(win_y,win_x))),axis=3)
-        windows = np.concatenate((windows,2*255+roll.window(data[i:win_y+i,:,2],(win_y,win_x))),axis=3)
-        windows = np.concatenate((windows,3*255+roll.window(data[i:win_y+i,:,3],(win_y,win_x))),axis=3)
-        windows = np.concatenate((windows,4*255+roll.window(data[i:win_y+i,:,4],(win_y,win_x))),axis=3)
-        windows = np.concatenate((windows,5*255+roll.window(data[i:win_y+i,:,5],(win_y,win_x))),axis=3)
-        windows = np.concatenate((windows,6*255+roll.window(data[i:win_y+i,:,6],(win_y,win_x))),axis=3)
-        windows = np.concatenate((windows,7*255+roll.window(data[i:win_y+i,:,7],(win_y,win_x))),axis=3)
-        windows = np.concatenate((windows,8*255+roll.window(data[i:win_y+i,:,8],(win_y,win_x))),axis=3)
-        windows = np.concatenate((windows,9*255+roll.window(data[i:win_y+i,:,9],(win_y,win_x))),axis=3)
+        windows = roll.rolling_window(data[i:win_y+i,:,0],(win_y,win_x)).astype(np.uint16)
+        windows = np.concatenate((windows,1*255+roll.rolling_window(data[i:win_y+i,:,1],(win_y,win_x))),axis=3)
+        windows = np.concatenate((windows,2*255+roll.rolling_window(data[i:win_y+i,:,2],(win_y,win_x))),axis=3)
+        windows = np.concatenate((windows,3*255+roll.rolling_window(data[i:win_y+i,:,3],(win_y,win_x))),axis=3)
+        windows = np.concatenate((windows,4*255+roll.rolling_window(data[i:win_y+i,:,4],(win_y,win_x))),axis=3)
+        windows = np.concatenate((windows,5*255+roll.rolling_window(data[i:win_y+i,:,5],(win_y,win_x))),axis=3)
+        windows = np.concatenate((windows,6*255+roll.rolling_window(data[i:win_y+i,:,6],(win_y,win_x))),axis=3)
+        windows = np.concatenate((windows,7*255+roll.rolling_window(data[i:win_y+i,:,7],(win_y,win_x))),axis=3)
+        windows = np.concatenate((windows,8*255+roll.rolling_window(data[i:win_y+i,:,8],(win_y,win_x))),axis=3)
+        windows = np.concatenate((windows,9*255+roll.rolling_window(data[i:win_y+i,:,9],(win_y,win_x))),axis=3)
         windows = np.squeeze(windows)
         for j in range(windows.shape[0]):
             features[i+int(win_y/2)+1,j+int(win_x/2)+2,0:8*D] = np.squeeze(cv2.calcHist([windows[i,:,:]],[0],None,[8*D],[0,256*D]))
@@ -105,30 +105,30 @@ def extract_features(data,win_y=15,win_x=15):
 
 #%% Classification
 
-def validation_split(features,lables,valPercent):
+def validation_split(features,labels,valPercent):
     ''' Split into validation and training sets
-        splits features and lables passed in as NxN matricies
+        splits features and labels passed in as NxN matricies
         returns validation and training matricies as N*N x 1 array with random order
     '''
     fN1,fN2,fC = np.shape(features)
-    lN1,lN2 = np.shape(lables)
-    #Data and lables must have same shape
+    lN1,lN2 = np.shape(labels)
+    #Data and labels must have same shape
     assert fN1 == lN1
     assert fN2 == lN2    
-    #place features and lables into flat arrays
+    #place features and labels into flat arrays
     features = np.reshape(features,(fN1*fN2,fC))
-    lables = lables.flatten()
-    #shuffle samples preserving lables
+    labels = labels.flatten()
+    #shuffle samples preserving labels
     order = list(range(np.shape(features)[0]))
     np.random.shuffle(order)
     features = features[order,:]
-    lables = lables[order]
+    labels = labels[order]
     #split data according to valPercent
     features_train = features[0:int(fN1*fN2*(1-valPercent))]
     features_val = features[int(fN1*fN2*(1-valPercent)) + 1:(fN1*fN2)]
-    lables_train = lables[0:int(fN1*fN2*(1-valPercent))]
-    lables_val = lables[int(fN1*fN2*(1-valPercent)) + 1:(fN1*fN2)]
-    return features_val,features_train,lables_val,lables_train
+    labels_train = labels[0:int(fN1*fN2*(1-valPercent))]
+    labels_val = labels[int(fN1*fN2*(1-valPercent)) + 1:(fN1*fN2)]
+    return features_val,features_train,labels_val,labels_train
 
 
 #%%  main
@@ -144,67 +144,101 @@ def main():
     fourier = get_fourier(gray)
     data = np.concatenate((data,hsv,gray,canny,fourier),axis=2)
     hsv,gray,canny,fourier = 0,0,0,0
-    '''
+    
     
     ## feature extraction ##
-    '''
+    
     data = extract_features(data)
     '''
     
-    # classification
-    data = np.load('../data/data_temp_features.npy')
-    data = data.reshape(data.shape[0]*data.shape[1],data.shape[2])
-    lda = LinearDiscriminantAnalysis()
-    lda.fit(data,np.ravel(labels))
-    X = lda.transform(data)
+# =============================================================================
+#     # classification
+#     data = np.load('../data/data_temp_features.npy')
+#     data = data.reshape(data.shape[0]*data.shape[1],data.shape[2])
+#     lda = LinearDiscriminantAnalysis()
+#     lda.fit(data,np.ravel(labels))
+#     X = lda.transform(data)
+#     
+# =============================================================================
     
+    custom_labels = load_custom_labels("../data/custom_labels.txt",np.shape(data));
     
-    '''
-    whitePix = np.where(pixel_class_labels == 1)
-    redPix = np.where(pixel_class_labels == 2)
-    poolPix = np.where(pixel_class_labels == 3)
-    pondPix = np.where(pixel_class_labels == 4)
-    bgPix = np.where(pixel_class_labels == -1)
+    bgPix = np.where(custom_labels == -1)
     bgPixCount = np.size(bgPix)/2
-    
+    data = data.reshape(data.shape[0]*data.shape[1],data.shape[2])
     #Split into training and validation
-    features_val,features_train,lables_val,lables_train = validation_split(features,pixel_class_labels,0)
-    
+    #shuffle samples preserving labels
+    order = list(range(np.shape(data)[0]))
+    np.random.shuffle(order)
+    data = data[order,:]
+    custom_labels = custom_labels[order]
     #Remove 99.9% of background data for training
     count = 0;
-    mask = np.ones(len(lables_train),dtype = bool)
-    for i in range(len(lables_train)):
-        if lables_train[i] == -1 and count < (0.995 * bgPixCount):
+    mask = np.ones(len(custom_labels),dtype = bool)
+    for i in range(len(custom_labels)):
+        if i%1000 == 0:
+            print(i)
+        if custom_labels[i] == -1 and count < (0.98 * bgPixCount):
             mask[i] = False
             count +=1
-    nobg_lables_train = lables_train[mask,...]
-    nobg_features_train = features_train[mask,...]
+    nobg_custom_labels = custom_labels[mask,...]
+    nobg_features = data[mask,...]
     
-    #Remove all of background data for testing
-    count = 0;
-    mask = np.ones(len(lables_train),dtype = bool)
-    for i in range(len(lables_train)):
-        if lables_train[i] == -1:
-            mask[i] = False
-            count +=1
-    nobg_lables_test = lables_train[mask,...]
-    nobg_features_test = features_train[mask,...]
     
-    #Predict lables or probability
-    predicted_lables = lda.predict(features_train)
+    #Predict labels or probability
+    lda = LinearDiscriminantAnalysis()
+    #predicted_labels = lda.predict(features_train)
     
-    lda.fit(nobg_features_train,nobg_lables_train)
-    lda.fit(features_train,lables_train)
+    lda.fit(nobg_features,nobg_custom_labels)
+    #lda.fit(features_train,labels_train)
     lda.predict([[-0.8,-1]])
     
-    predicted_lables = np.reshape(predicted_lables,(int(predicted_lables.size**(1/2)),int(predicted_lables.size**(1/2))))
-    write_pred_data(data_train,predicted_lables)
-    '''
+    predicted_labels = np.reshape(predicted_labels,(int(predicted_labels.size**(1/2)),int(predicted_labels.size**(1/2))))
+    
+    predicted_labels = np.reshape(predicted_labels,(6250,6250))
+    
+    data_train,a,b,c = load_train_data()
+    
+    data_train[predicted_labels==1,:] = [255,0,255]
+    cv2.imwrite('../data/data_pred_labels.png',data_train[:,:,::-1])
+    
+    write_pred_data(data_train,predicted_labels)
+    
+    
+    #fuck it
+    #post processing shit
+    
+    img,locs,labels,pond_masks = load_train_data()
+    row = 
+    col = 
+    classed = 
+    for i in range(len(row)):
+        for j in range(len(col)):
+            if (classed[i,j] == redcar):
+                if (img[i,j,0] >= 106 and img[i,j,0] <= 40 and img[i,j,1] >= 69 and img[i,j,1] >= 40 and img[i,j,2] >= 95 and img[i,j,2] <= 59):
+                    classed[i,j] = redcar;
+                else:
+                    classed[i,j] = background;
+            elif (classed[i,j] == whitecar):
+                if (img[i,j,0] >= 227 and img[i,j,0] <= 255 and img[i,j,1] >= 237 and img[i,j,1] >= 255 and img[i,j,2] >= 230 and img[i,j,2] <= 255):
+                    classed[i,j] = whitecar;
+                else:
+                    classed[i,j] = background;
+            elif (classed[i,j] == pool):
+                if (img[i,j,0] >= 32 and img[i,j,0] <= 101 and img[i,j,1] >= 135 and img[i,j,1] >= 197 and img[i,j,2] >= 151 and img[i,j,2] <= 204):
+                    classed[i,j] = pool;
+                else:
+                    classed[i,j] = background;
+            elif (classed[i,j] == pond):
+                if (img[i,j,0] >= 36 and img[i,j,0] <= 90 and img[i,j,1] >= 40 and img[i,j,1] >= 116 and img[i,j,2] >= 43 and img[i,j,2] <= 84):
+                    classed[i,j] = pond;
+                else:
+                    classed[i,j] = background;
 
     #Score
     '''
-    f1score = f1_score(nobg_lables_train,predicted_lables,average = 'micro')
-    #f1score = f1_score(nobg_lables_test,predicted_lables,average = 'micro')
+    f1score = f1_score(nobg_labels_train,predicted_labels,average = 'micro')
+    #f1score = f1_score(nobg_labels_test,predicted_labels,average = 'micro')
     print('f1 score = ' + f1score)
     '''
 
--- code/util.py
+++ code/util.py
@@ -56,10 +56,16 @@ def write_pred_data(data_train,labels):
     N1, N2 = np.shape(labels)
     for i in range(N1):
         for j in range(N2):
-            if labels[i,j] != -1:
-               data_pred_labels[i,j,:] = [255,0,255] 
-              
-               
+            print(j)
+            if labels[i,j] == 1:
+               data_pred_labels[i,j,:] = [0,255,0]
+            elif labels[i,j] == 2:
+               data_pred_labels[i,j,:] = [0,255,255]
+            elif labels[i,j] == 3:
+               data_pred_labels[i,j,:] = [255,255,0]
+            elif labels[i,j] == 4:
+               data_pred_labels[i,j,:] = [255,0,0]
+                           
     cv2.imwrite('../data/data_pred_labels.png',data_pred_labels[:,:,::-1])
     
 
